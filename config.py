22315735b3ace164a6cbdbea1438534f531b57ff
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-08-12 11:30:46 +0300

    Added config.py file

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY'
+    EVERYTHING_SOURCE_BASE_URL = ''
+    TOP_HEADLINES_BASE_URL = ''
+    EVERYTHING_BASE_URL = ''
+    SEARCH_API_BASE_URL = ''
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
