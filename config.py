842fb371297fc17a1e312563a996998edc5e55ac
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-08-12 12:41:14 +0300

    Added the urls

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config:
     General configuration parent class
     '''
     SOURCE_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY'
-    EVERYTHING_SOURCE_BASE_URL = ''
-    TOP_HEADLINES_BASE_URL = ''
-    EVERYTHING_BASE_URL = ''
-    SEARCH_API_BASE_URL = ''
+    EVERYTHING_SOURCE_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY'
+    TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY'
+    EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=b1c90d00a6e14923aed224401a3aaa61'
+    SEARCH_API_BASE_URL = 'https://newsapi.org/v2/everything?q=Apple&from=2021-08-12&sortBy=popularity&apiKey=API_KEY'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
